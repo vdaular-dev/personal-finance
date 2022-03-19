499792740419c1dc23eedb52e08ef61841bb3cb6
davidebuonincontro <davide.buonincontro@unipegaso.it>
2022-03-19 13:12:23 +0100

    first commit

---
+++ src/test/java/com/personal_finance/personal_finance/PersonalFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal_finance.personal_finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalFinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
