8e978f77a579cfa9ea9f734f036e3305bacf6785
buoni <dbuonincontro@gmail.com>
2022-07-26 00:16:29 +0200

    Fix

--- src/main/java/com/dave/personal_finance/AccountType/AccountTypeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dave.personal_finance.AccountType;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AccountTypeRepository extends JpaRepository<AccountType,Long>{
-    
-}
